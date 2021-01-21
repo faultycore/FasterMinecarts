1880c8a21163c255f0c917636e7ce585d1f0e769
SFort <github@ssf.tf>
2021-01-21 18:48:57 +0100

    Extreamly simplified code that works with diagonal rails

--- src/main/java/com/github/ndrp/fasterminecarts/mixin/AbstractMinecartEntityMixin.java
+++ src/main/java/com/github/ndrp/fasterminecarts/mixin/AbstractMinecartEntityMixin.java
@@ -1,7 +1,6 @@
 package com.github.ndrp.fasterminecarts.mixin;
 
-import net.minecraft.block.AbstractRailBlock;
-import net.minecraft.block.BlockState;
+import net.minecraft.block.*;
 import net.minecraft.block.enums.RailShape;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
@@ -11,6 +10,7 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -18,63 +18,18 @@ import com.github.ndrp.fasterminecarts.MinecartUtility;
 
 @Mixin(AbstractMinecartEntity.class)
 public abstract class AbstractMinecartEntityMixin extends Entity {
-	
-	// Needed because Entity has no default constructor
 	public AbstractMinecartEntityMixin(EntityType<?> type, World world) {
 		super(type, world);
 	}
 	
 	@Inject(method = "getMaxOffRailSpeed", at = @At("HEAD"), cancellable = true)
 	protected void onGetMaxOffRailSpeed(CallbackInfoReturnable<Double> cir) {
-		
-		final double topSpeed = 1.6;
-		
-		Vec3d v = this.getVelocity();
-		
-		if (Math.abs(v.getX()) < 0.5 && Math.abs(v.getZ()) < 0.5) { // return early if at a speed where we can't possibly derail
-			cir.setReturnValue(topSpeed);
-			return;
-		}
-		
-		BlockPos blockPos = this.getBlockPos();
-		BlockState state = this.world.getBlockState(blockPos);
-		
-		if (state.getBlock() instanceof AbstractRailBlock) {
-			final double slowdownSpeed = 0.3;
-			final int additionalOffset = 1;
-			final int offset = (int)topSpeed + additionalOffset;
-			
-			AbstractRailBlock abstractRailBlock = (AbstractRailBlock)state.getBlock();
-			RailShape railShape = (RailShape)state.get(abstractRailBlock.getShapeProperty());
-			Vec3i nextRailOffset = MinecartUtility.getNextRailOffsetByVelocity(railShape, v);
-			
-			if (nextRailOffset == null) {
-				cir.setReturnValue(slowdownSpeed);
-				return;
-			}
-				
-			for (int i = 0; i <= offset; i++) {	
-				RailShape railShapeAtOffset = null;
-				
-				railShapeAtOffset = MinecartUtility.getRailShapeAtOffset(new Vec3i(nextRailOffset.getX() * i, 0, nextRailOffset.getZ() * i), blockPos, this.world);
-				
-				if (railShapeAtOffset == null) {
-					cir.setReturnValue(slowdownSpeed);
-					return;
-				}
-				
-				switch (railShapeAtOffset) {
-				case SOUTH_EAST:
-				case SOUTH_WEST:
-				case NORTH_WEST:
-				case NORTH_EAST:
-					cir.setReturnValue(slowdownSpeed);
-					return;
-				default:
-				}	
-			}
-			cir.setReturnValue(topSpeed);
-		} 
+		Block under = world.getBlockState(getBlockPos().down()).getBlock();
+		//Can't place rails on honey blocks
+		if(!(world.getBlockState(getBlockPos()).getBlock() instanceof RailBlock) || under instanceof SoulSandBlock)
+			cir.setReturnValue(0.3);
+		else
+			cir.setReturnValue(1.6);
 	}
 }
 
