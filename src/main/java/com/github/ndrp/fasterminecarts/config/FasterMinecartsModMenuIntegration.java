989773da7c2fe794e3af1e1ffcdc8f5374f7b096
Falconjh <Hutchinsjh@hotmail.com>
2021-11-01 19:49:21 -0600

    fix blocks/second; update to 1.17

--- src/main/java/com/github/ndrp/fasterminecarts/config/FasterMinecartsModMenuIntegration.java
+++ src/main/java/com/github/ndrp/fasterminecarts/config/FasterMinecartsModMenuIntegration.java
@@ -1,18 +1,13 @@
 package com.github.ndrp.fasterminecarts.config;
 
-import io.github.prospector.modmenu.api.ConfigScreenFactory;
-import io.github.prospector.modmenu.api.ModMenuApi;
-import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
 @Environment(EnvType.CLIENT)
-public class FasterMinecartsModMenuIntegration implements ModMenuApi {	
-	
-    @Override
-    public String getModId() {
-        return "faster-minecarts";
-    }
+public class FasterMinecartsModMenuIntegration implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
